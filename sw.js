1a60024d8998e1c349f9e4262c29db33d7eff805
jtaaa <joshuatallum@gmail.com>
2019-03-23 17:36:30 -0400

    Add initial service worker setup

---
+++ sw.js
@@ -0,0 +1,25 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js');
+
+console.log('I know it\'s me because who else would say this.');
+
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('I\'m a service worker and I saw a fetch!');
+})
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+workbox.precaching.precacheAndRoute([], {});
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{json,ico,html,js,css,svg}"
+  ],
+  "swDest": "build/service-worker.js",
+  "swSrc": "sw.js"
+};
