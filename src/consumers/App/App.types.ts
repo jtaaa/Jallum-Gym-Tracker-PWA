1777a279061b833cbd2086df9669b7ca1df56abd
jtaaa <joshuatallum@gmail.com>
2019-03-24 17:49:52 -0400

    Define AppReduxProps for better typing

--- src/consumers/App/App.types.ts
+++ src/consumers/App/App.types.ts
@@ -3,11 +3,22 @@ import { CallHistoryMethodAction } from 'connected-react-router';
 
 import { SessionsAction } from './../../redux/sessions';
 import { OptionsListOptions } from '../../components/OptionsList/OptionsList.types';
+import { Exercise, ExercisesAction } from '../../redux/exercises';
 
-export interface AppProps {
-  location: Location,
+export interface AppReduxDispatchProps {
   navigateTo: (route: string) => CallHistoryMethodAction,
   startSession: (muscleGroups: Array<string>) => SessionsAction,
+  addExercises: (exercises: Array<Exercise>) => ExercisesAction,
+};
+
+export interface AppReduxStateProps {
+  location: Location;
+  exercisesOptions: OptionsListOptions;
+};
+
+export type AppReduxProps = AppReduxStateProps & AppReduxDispatchProps;
+
+export interface AppProps extends AppReduxProps {
 };
 
 export interface AppState {
@@ -15,4 +26,5 @@ export interface AppState {
   exercise?: string;
   reps: number;
   weight: number;
-}
+};
+
