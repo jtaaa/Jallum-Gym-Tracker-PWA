db0b4a85b6cbb3f575408408d77fe9c5d59db7ab
jtaaa <joshuatallum@gmail.com>
2019-03-24 16:46:17 -0400

    Use exercisesReducer on exercises prop on store

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -3,10 +3,12 @@ import { History } from 'history';
 import { connectRouter } from 'connected-react-router';
 
 import { sessionsReducer } from './sessions/sessions.reducer';
+import { exercisesReducer } from './exercises/exercises.reducer';
 
 const createRootReducer = (history: History<any>) => combineReducers({
   router: connectRouter(history),
   sessions: sessionsReducer,
+  exercises: exercisesReducer,
 });
 
 export { createRootReducer };
