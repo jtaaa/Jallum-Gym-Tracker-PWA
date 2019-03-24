de8dcf3a7a9ef91674a6aead0255fc78d3bdf380
jtaaa <joshuatallum@gmail.com>
2019-03-24 16:53:23 -0400

    Add exercises property and type to State type

--- src/redux/state.types.ts
+++ src/redux/state.types.ts
@@ -1,7 +1,9 @@
 import { RouterState } from 'connected-react-router';
 import { SessionsState } from './sessions';
+import { ExercisesState } from './exercises';
 
 export interface State {
   router: RouterState,
   sessions: SessionsState,
+  exercises: ExercisesState,
 };
