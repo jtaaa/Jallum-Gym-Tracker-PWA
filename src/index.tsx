d7f7505fe968e0bcb78d055f8c754448870ea91a
jtaaa <joshuatallum@gmail.com>
2019-03-23 21:00:05 -0400

    Add Provider and ConnectedRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,18 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { store, history } from './redux/store';
+import { ConnectedRouter } from 'connected-react-router';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
