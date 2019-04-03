f964a134a39819848013c810e65ba0e3d78f4f4f
jtaaa <joshuatallum@gmail.com>
2019-04-02 23:06:44 -0400

    Add cancel IconButton icon

--- src/components/IconButton/IconButton.types.ts
+++ src/components/IconButton/IconButton.types.ts
@@ -2,7 +2,7 @@ import { MouseEvent } from 'react';
 
 export interface IconButtonProps {
   handleClick?: (event: MouseEvent<HTMLDivElement>) => void;
-  icon: 'add' | 'minus' | 'next' | 'start' | 'done';
+  icon: 'add' | 'minus' | 'next' | 'start' | 'done' | 'cancel';
   outline?: 'none' | 'dashed';
   margin?: boolean;
   stopPropagation?: boolean;
